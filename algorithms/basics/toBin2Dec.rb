bf54bb996472f321eeab627f05ac9d9eb56fc47e
Adrian Statescu <mergesortv@gmail.com>
2023-02-16 09:53:43 +0200

    Update toBin2Dec.rb

--- algorithms/basics/toBin2Dec.rb
+++ algorithms/basics/toBin2Dec.rb
@@ -31,6 +31,25 @@ def toBin5(n)
     int.reverse_each{|i| print (n >> i) & 1}
 end
 
+def power(a, b)
+  p = 1
+  for i in 1..b
+      p = p * a
+  end
+  p
+end
+
+def toDec(n)
+    dec = 0
+    power = 0
+    until n == 0
+      dec += n % 10 * power(2, power)
+      n /= 10
+      power+=1
+    end
+    dec
+end
+
 def func()
     n = 8
     toBin(n)
@@ -42,5 +61,9 @@ def func()
     toBin4(n)
     print"\n"
     toBin5(n)
+    print"\n"
+    for i in 1000..1010
+      p toDec(i)
+    end
 end
 func()
